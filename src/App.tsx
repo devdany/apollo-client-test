d4baed117b89421a6d558fa759222ba6c761333a
devdany <dayong7387@gmail.com>
2023-06-11 14:35:15 +0900

    mutation test 준비 UI구성

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,58 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { gql, useQuery } from '@apollo/client';
 
-const POST_QUERY = gql`
-  query post($id: ID!) {
-    post(id: $id) {
+const POSTS = gql`
+  query {
+    posts {
       id
       title
+      content
     }
   }
 `
 
 function App() {
-  const { data, loading, error } = useQuery(POST_QUERY, {
-    variables: {
-      id: 'post-1'
-    }
-  })
+  const [title, setTitle] = React.useState<string>();
+  const [content, setContent] = React.useState<string>();
 
-  if (loading) {
-    return <div>Loading...</div>
-  }
-
-  if (!data || error) {
-    return <div>Error!</div>
+  const { data, loading, error } = useQuery(POSTS);
+  
+  const handleClickAddButton = () => {
+    // 여기서 mutation을 요청하세요.
   }
   return (
     <div className="App">
-      my post title is {data.post.title}
+      <table>
+        <tr>
+          <th>제목</th>
+          <th>작성자</th>
+        </tr>
+        {loading ? (
+          <span>로딩중..</span>
+        ) : (
+          !data || error ? (
+            <span>데이터를 가져오지 못했습니다.</span>
+          ) : (
+            data.posts.map((post: any) => (
+              <tr>
+                <td>{post.title}</td>
+                <td>{post.content}</td>
+              </tr>
+            ))
+          )
+        )}
+        
+      </table>
+      <div style={{ border: '1px solid #ddd', display: 'flex', flexDirection: 'column', marginTop: '20px', padding: '12px', width: '500px' }}>
+        <div>
+          제목: <input value={title} onChange={(e) => setTitle(e.target.value)} />
+        </div>
+        <div>
+          내용: <textarea value={content} onChange={(e) => setContent(e.target.value)} />
+        </div>
+        <button onClick={handleClickAddButton}>포스트 추가</button>
+      </div>
     </div>
   );
 }
