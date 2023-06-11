3432ad3f77ffab9bb9559ef3674a3a7552569e71
devdany <dayong7387@gmail.com>
2023-06-11 14:01:42 +0900

    apollo client로 post query 요청하기

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,34 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { gql, useQuery } from '@apollo/client';
+
+const POST_QUERY = gql`
+  query post($id: ID!) {
+    post(id: $id) {
+      id
+      title
+    }
+  }
+`
 
 function App() {
- 
+  const { data, loading, error } = useQuery(POST_QUERY, {
+    variables: {
+      id: 'post-1'
+    }
+  })
+
+  if (loading) {
+    return <div>Loading...</div>
+  }
+
+  if (!data || error) {
+    return <div>Error!</div>
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      my post title is {data.post.title}
     </div>
   );
 }
